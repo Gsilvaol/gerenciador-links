2021ef24e941af080b28f86bc404394e4e67efbc
Gsilvaol <gustavosilva1994_@hotmail.com>
2025-01-20 14:36:30 -0300

    navegando

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -6,6 +6,8 @@ import { colors } from "@/styles/colors";
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
 import { Option } from "@/components/option";
+import { router } from "expo-router";
+
 
 export default function Index() {
     return (
@@ -13,14 +15,14 @@ export default function Index() {
             <View style={styles.header}>
                 <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => router.navigate("/add")}>
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
+            
+            <Categories/> 
 
-            <Categories />
-
-            <FlatList
+            <FlatList 
                 data={["1", "2", "3", "4", "5"]}
                 keyExtractor={item => item}
                 renderItem={({ item }) => (
@@ -35,7 +37,7 @@ export default function Index() {
                 showsVerticalScrollIndicator={false}
             />
 
-            <Modal transparent visible={true}>
+            <Modal transparent visible={false}>
                 <View style={styles.modal}>
                     <View style={styles.modalContent}>
                         <View style={styles.modalHeader}>
