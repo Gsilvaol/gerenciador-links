99c8dcba651fb5e89a7b366ec00f298f39004128
Gustavo <gustavosilva1994_@hotmail.com>
2025-01-23 15:31:41 -0300

    criando estados

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -6,27 +6,35 @@ import { router } from "expo-router";
 import { Categories } from "@/components/categories";
 import { Input } from "@/components/input";
 import { Button } from "@/components/button";
+import { useState } from "react";
 
 export default function Add() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <TouchableOpacity onPress={() => router.back()}>
-                    <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]} />
-                </TouchableOpacity>
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
 
-                <Text style={styles.title}>Novo</Text>
-            </View>
+  function handleAdd() {
+    console.log({ name, url });
+  }
 
-            <Text style={styles.label}>Selecione uma categoria</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.back()}>
+          <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]} />
+        </TouchableOpacity>
 
-            <Categories />
+        <Text style={styles.title}>Novo</Text>
+      </View>
 
-            <View style={styles.form}>
-                <Input placeholder="Nome" onChangeText={console.log} />
-                <Input placeholder="Url" />
-                <Button title="Adicionar" />
-            </View>
-        </View>
-    )
+      <Text style={styles.label}>Selecione uma categoria</Text>
+
+      <Categories />
+
+      <View style={styles.form}>
+        <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
+        <Input placeholder="Url" onChangeText={setUrl} autoCorrect={false} />
+        <Button title="Adicionar" onPress={handleAdd} />
+      </View>
+    </View>
+  );
 }
