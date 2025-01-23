3f40aa78cee3627868571e48189de3796bca40f0
Gustavo <gustavosilva1994_@hotmail.com>
2025-01-23 16:04:06 -0300

    selecionando a categoria

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,5 +1,12 @@
-import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons"
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Text,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
@@ -7,59 +14,64 @@ import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
 import { Option } from "@/components/option";
 import { router } from "expo-router";
-
+import { useState } from "react";
+import { categories } from "@/utils/categories";
 
 export default function Index() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Image source={require("@/assets/logo.png")} style={styles.logo} />
+  const [category, setCategory] = useState(categories[0].name);
 
-                <TouchableOpacity onPress={() => router.navigate("/add")}>
-                    <MaterialIcons name="add" size={32} color={colors.green[300]} />
-                </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+        <TouchableOpacity onPress={() => router.navigate("/add")}>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
+
+      <Categories onChange={setCategory} selected={category} />
+
+      <FlatList
+        data={["1", "2", "3", "4", "5"]}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Link
+            name="Rocketseat"
+            url="https://www.rocketseat.com.br/"
+            onDetails={() => console.log("Clicou")}
+          />
+        )}
+        style={styles.links}
+        contentContainerStyle={styles.linksContent}
+        showsVerticalScrollIndicator={false}
+      />
+
+      <Modal transparent visible={false}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
             </View>
-            
-            <Categories/> 
 
-            <FlatList 
-                data={["1", "2", "3", "4", "5"]}
-                keyExtractor={item => item}
-                renderItem={({ item }) => (
-                    <Link
-                        name="Rocketseat"
-                        url="https://www.rocketseat.com.br/"
-                        onDetails={() => console.log("Clicou")}
-                    />
-                )}
-                style={styles.links}
-                contentContainerStyle={styles.linksContent}
-                showsVerticalScrollIndicator={false}
-            />
+            <Text style={styles.modalLinkName}>Rocketseat</Text>
 
-            <Modal transparent visible={false}>
-                <View style={styles.modal}>
-                    <View style={styles.modalContent}>
-                        <View style={styles.modalHeader}>
-                            <Text style={styles.modalCategory}>Curso</Text>
-                            <TouchableOpacity>
-                                <MaterialIcons name="close" size={20} color={colors.gray[400]} />
-                            </TouchableOpacity>
-                        </View>
+            <Text style={styles.modalUrl}>https://www.rocketseat.com.br/</Text>
 
-                        <Text style={styles.modalLinkName}>Rocketseat</Text>
-
-                        <Text style={styles.modalUrl}>https://www.rocketseat.com.br/</Text>
-
-                        <View style={styles.modalFooter}>
-                            <Option name="Excluir" icon="delete" variant="secondary" />
-                            <Option name="Abrir" icon="language"  />
-                        </View>
-                    </View>
-
-                </View>
-            </Modal>
+            <View style={styles.modalFooter}>
+              <Option name="Excluir" icon="delete" variant="secondary" />
+              <Option name="Abrir" icon="language" />
+            </View>
+          </View>
         </View>
-    )
+      </Modal>
+    </View>
+  );
 }
-
