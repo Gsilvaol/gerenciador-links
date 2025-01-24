7eadcf593b7a021f98dafae916e6cb055a48919e
Gustavo <gustavosilva1994_@hotmail.com>
2025-01-24 15:01:23 -0300

    utilizando useEffect

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -5,6 +5,7 @@ import {
   FlatList,
   Modal,
   Text,
+  Alert,
 } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
@@ -14,11 +15,26 @@ import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
 import { Option } from "@/components/option";
 import { router } from "expo-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { categories } from "@/utils/categories";
+import { linkStorage, LinkStorage } from "@/storage/link-storage";
 
 export default function Index() {
+  const [links, setLinks] = useState<LinkStorage[]>([]);
   const [category, setCategory] = useState(categories[0].name);
+
+  async function getLinks() {
+    try {
+      const response = await linkStorage.get();
+      setLinks(response);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possivel listar os links");
+    }
+  }
+
+  useEffect(() => {
+    getLinks();
+  }, [category]);
 
   return (
     <View style={styles.container}>
@@ -33,12 +49,12 @@ export default function Index() {
       <Categories onChange={setCategory} selected={category} />
 
       <FlatList
-        data={["1", "2", "3", "4", "5"]}
-        keyExtractor={(item) => item}
+        data={links}
+        keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <Link
-            name="Rocketseat"
-            url="https://www.rocketseat.com.br/"
+            name={item.name}
+            url={item.url}
             onDetails={() => console.log("Clicou")}
           />
         )}
